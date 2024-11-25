e4e71d4c8e5139fb7031201c0de99201c03ba295
tugcetorunn <tokerrtugcee@gmail.com>
2024-11-25 18:01:06 +0300

    Publisher was created with RabbitMQ.Client 7.0.0 and Consumer was created with 6.4.0 version. This is simple RabbitMQ content. To be continue.

--- SimpleRabbitMQ.Consumer/Program.cs
+++ SimpleRabbitMQ.Consumer/Program.cs
@@ -1,10 +1,44 @@
-﻿
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
+// rabbitmq.client 6.4 versiyonu ile yapalım.
+
+// 1. adım - Bağlantı oluşturma
 ConnectionFactory factory = new();
 factory.Uri = new("amqps://efithflb:8XLVmInfj3Q37bnvn6yBCUb_1upLf86Y@shrimp.rmq.cloudamqp.com/efithflb");
 
+// 2. adım - Bağlantıyı aktifleştirme ve kanal açma
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "example-queue", exclusive: false);
+// 3. adım - Queue oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false); // parametreler de birebir aynı olması false sa false gitli yoksa tutarsızlık olur
+                                                                // hata meydana gelir.
+
+// 4. adım - Queue dan mesaj okuma
+// kuyruktan okuma yapmamız için bu channel üzerinde bir event operasyonu yapmamız gerek.
+EventingBasicConsumer consumer = new(channel); // bu class parametre olarak IModel türünden obje ister. çünkü işlemlerini bu channel üzerinden yapacak.
+// consumer ismindeki bu instance üzerinde bu channel da bildireceğimiz kuyrukta bir mesaj olursa onu receive (?consume) edeceğiz (alıp işleyeceğiz). 
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer: consumer); // autoack: kuyruktan mesajı aldığımızda silinip silinmemesini
+                                                                                  // belirlediğimiz özellik.
+                                                                                  
+consumer.Received += (sender, e) => // mesaj geldiği anda yakalaması (receive) için recieved kullanılır. 
+// delegate, bir yöntemi bir değer olarak ele almanıza, yöntemi bir değişkene atamanıza, diğer yöntemlere parametre olarak geçirmenize, bir koleksiyona
+// eklemenize vb. olanak tanıyan nesnedir. += () =>
+// https://stackify.com/c-delegates-definition-types-examples/
+{
+    // kuyruğa gelen mesajın işlendiği yerdir.
+    // e.Body: kuyruktaki mesajın verisini büyünsel olarak getirir.
+    // e.Body.Span veya e.Body.ToArray: kuyruktaki mesajın byte verisini getirir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.ReadLine();
+
+// consumer.Received += Consumer_Received;
+   
+// void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+// {
+//     throw new NotImplementedException();
+// } // bu şekilde metodlaştırılabilir, .net otomatik oluşturdu.
--- SimpleRabbitMQ.Publisher/Program.cs
+++ SimpleRabbitMQ.Publisher/Program.cs
@@ -1,9 +1,9 @@
-﻿
-
-// 1. adım - Bağlantı oluşturma
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
 using System.Text;
 
+// rabbitmq.client 7 sürümü async metodlar içermekte. 6.4 async metodlar içermemektedir.
+
+// 1. adım - Bağlantı oluşturma
 ConnectionFactory factory = new();
 
 // factory.HostName = "guest"; // varsa hostname ve password bilgileri ile de bağlanılabilir.
@@ -12,13 +12,11 @@ ConnectionFactory factory = new();
 factory.Uri = new("amqps://efithflb:8XLVmInfj3Q37bnvn6yBCUb_1upLf86Y@shrimp.rmq.cloudamqp.com/efithflb"); // amqp  ile bağlanıyoruz.
 
 // 2. adım - Bağlantıyı aktifleştirme ve kanal açma
-
 using IConnection connection = factory.CreateConnectionAsync().Result; // IConnection IDisposable interface i olduğu için using ile işaretliyoruz ki işlem
                                                                        // tamamlandıktan sonra bu nesne dispose adilip bellekte temizlenmiş olsun. 
 using IChannel channel = connection.CreateChannelAsync().Result;
 
 // 3. adım - Queue oluşturma
-
 await channel.QueueDeclareAsync(queue: "example-queue", exclusive: false); // durable: kuyruktaki mesajların kalıcığıyla alakalı bir configuration sağlar.
                                                                            // exclusive: bu kuyruğun özel olup olmadığını yani bu kuyrukta birden fazla işlem yapıp
                                                                            // yapamayacağımızı belirlediğimiz bir özellik. default true dur true olduğunda bu
@@ -30,11 +28,30 @@ await channel.QueueDeclareAsync(queue: "example-queue", exclusive: false); // du
                                                                            // autoDelete: kuyruğun içindeki tüm mesajlar tüketildikten sonra kuyruğun otomatik
                                                                            // silinip silinmeyeceğini belirlediğimiz özellik.
 
-// 4. adım - Kuyruğa mesaj gönderme
+// 4. adım - Queue ya mesaj gönderme
 // rabbitmq kuyruğa atacağı mesajları byte türünden kabul ediyor.
+// byte[] message = Encoding.UTF8.GetBytes("merhaba");
+// await channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: message); // exchane belirtmedik bu şekilde default olan direct exchange
+// işlem yapacak. bu exchange e göre de routing key = queue ismi
 
-byte[] message = Encoding.UTF8.GetBytes("merhaba");
-await channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: message); // exchane belirtmedik bu şekilde default olan direct exchange
-                                                                                           // işlem yapacak. bu exchange e göre de routing key = queue ismi
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(300); // mesajların nasıl geldiğini görebilmek adına ekledik.
+    byte[] message = Encoding.UTF8.GetBytes("merhaba " + i);
+    await channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: message);
+}
+
+// bu şekilde consumer ı da oluşturduktan sonra ikisini de aynı anda çağırdığımızda console da arka arkaya mesajların sırayla geldiğini görürüz.
 
 Console.ReadLine();
+
+// rabbitmq.client 6.4 ile yazılırsa;
+// using RabbitMQ.Client;
+   
+// ConnectionFactory factory = new();
+// factory.Uri = new("amqps://efithflb:8XLVmInfj3Q37bnvn6yBCUb_1upLf86Y@shrimp.rmq.cloudamqp.com/efithflb");
+   
+// using IConnection connection = factory.CreateConnection();
+// using IModel channel = connection.CreateModel();
+   
+// channel.QueueDeclare(queue: "example-queue", exclusive: false);
